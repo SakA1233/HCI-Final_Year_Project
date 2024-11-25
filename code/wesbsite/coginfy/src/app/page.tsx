d9ce4789da9bb308850c2429edfa8879a7e5660d
Sakariya_Aden <zlac406@live.rhul.ac.uk>
2024-11-25 15:16:12 +0000

    added footer to page.

--- code/wesbsite/coginfy/src/app/page.tsx
+++ code/wesbsite/coginfy/src/app/page.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import NavbarComponent from "@/app/components/navBar";
+import React from 'react';
+import Head from 'next/head';
+import Navbar from '@/app/components/navBar';
+import Footer from '@/app/components/footer';
 
 export default function Home() {
   return (
     <>
-      <NavbarComponent />
-
-      <title>Cognify - Empowering Minds</title>
-      <meta name="description" content="Empowering minds one step at a time" />
+      <Head>
+        <title>Cognify - Empowering Minds</title>
+        <meta name="description" content="Empowering minds one step at a time" />
+      </Head>
+      <Navbar />
       <main className="text-center py-16 px-8">
         <h2 className="text-5xl font-bold mb-4">Cognify</h2>
         <p className="text-xl mb-6">Empowering minds one step at a time</p>
       </main>
+      <Footer />
     </>
   );
 }
